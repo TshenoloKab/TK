06aa9d6d85f7ede4c5d637d3371d9c70875fe218
Kate2030 <tckabikwa@yahoo.com>
2023-09-17 16:02:49 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+
+    const menu = document.querySelector('.hum-menu-links');
+    const icons = document.querySelector('.nav__hum-icons');
+    menu.classList.toggle('open');
+    icons.classList.toggle('open');
+}
